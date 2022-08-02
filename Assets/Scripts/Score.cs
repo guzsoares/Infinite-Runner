74aaa2a89a7c6221cfa69b9b9715a0dc057904ed
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-08-02 17:34:35 -0300

    ttt

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,12 +6,13 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public BackgroundScroller bd;
 
     private void Start() {
         text = GetComponent<TextMeshProUGUI>();
     }
 
     private void Update(){
-        text.SetText("Score: " + ((int)Time.time).ToString());
+        text.SetText("Score: " + ((int)Time.time  * (int)(bd.scrollSpeed/4)).ToString());
     }
 }
